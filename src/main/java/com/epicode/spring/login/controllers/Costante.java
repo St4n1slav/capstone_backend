43ae0d92c7c504a6497b5a3d5020b7d54aefcb74
St4n1slav <stanyyaro@gmail.com>
2025-06-11 03:47:23 +0200

    hotfix

--- src/main/java/com/epicode/spring/login/controllers/Costante.java
+++ src/main/java/com/epicode/spring/login/controllers/Costante.java
@@ -2,5 +2,5 @@ package com.epicode.spring.login.controllers;
 
 public class Costante {
 public static final String fakeusername = "user@epicode.it";
-public static final String fakepassword = "pass";
+public static final String fakepassword = "password";
 }
